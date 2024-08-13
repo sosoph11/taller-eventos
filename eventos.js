8b8d35912d725eff63925f93413fcba9b7182126
Sofía <sofiadsdemello@gmail.com>
2024-08-13 09:05:50 -0300

    Actualizar HTML y eventos.js para manejar la propagación del clic

--- eventos.js
+++ eventos.js
@@ -2,3 +2,7 @@ document.getElementById('miDiv').addEventListener('click', function() {
     alert('Hola! Soy el div');
 });
 
+document.querySelector('button').addEventListener('click', function(event) {
+    event.stopPropagation(); // Detiene la propagación del clic al div
+    alert('Hola!');
+});
